0766f4655c884aa5cc0de5e9ecef1bf8a883ca7d
joyal0 <76865773+joyal0@users.noreply.github.com>
2023-01-15 12:16:15 +0530

    2

--- Problem1.cpp
+++ Problem1.cpp
@@ -4,9 +4,11 @@ using namespace std;
 int main()
 {
     int sum = 0;
-    for(int i=2;(i%3==0)&&(i%5==0)&&(i<10000);++i)
+    int i;
+    for(i=3;(i<10000);++i)
     {
-        sum += i;
+        if((i%3==0)||(i%5==0))
+            sum += i;
     }
     cout<<"Sum = "<<sum;
     return 0;
